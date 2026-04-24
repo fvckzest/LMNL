be5895475c63ab97b7da27a5cbd73aa5f469896c
zest <fvck@zest.art>
2026-04-23 22:35:02 -0700

    feat: simplify admin subdomain routing (root is now dashboard)

--- src/App.jsx
+++ src/App.jsx
@@ -40,41 +40,46 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* PUBLIC ROUTES - Always accessible */}
-        <Route path="/" element={<Home />} />
+        {/* SHARED / PUBLIC CONTENT */}
         <Route path="/events" element={<Events />} />
-        <Route path="/services" element={<GenericPage title="SERVICES" color="#6222d8" />} />
-        <Route path="/community" element={<GenericPage title="COMMUNITY" color="#ff5bb8" />} />
-        <Route path="/shop" element={<GenericPage title="SHOP" color="#ff0000" />} />
+        <Route path="/space" element={<Space />} />
         <Route path="/about" element={
           <GenericPage title="ABOUT" color="#ff9300">
             <img src="/rules.png" alt="Rules" className="about-rules-img" />
           </GenericPage>
         } />
-        <Route path="/blog" element={<GenericPage title="BLOG" color="#ffde00" />} />
-        <Route path="/contact" element={<GenericPage title="CONTACT" color="#90e937" />} />
-        <Route path="/prsm" element={<GenericPage title="PRSM" color="#000000" />} />
-        <Route path="/space" element={<Space />} />
 
-        {/* ADMIN ROUTES - Only accessible via admin subdomain or localhost */}
+        {/* SUBDOMAIN SPECIFIC LOGIC */}
         {showAdmin ? (
           <>
-            <Route path="/login" element={<Login />} />
-            <Route path="/admin" element={
+            {/* On admin.lmnl.art, the root is the Dashboard */}
+            <Route path="/" element={
               <ProtectedRoute>
                 <Admin />
               </ProtectedRoute>
             } />
+            {/* Allow viewing the public home via /home on the subdomain */}
+            <Route path="/home" element={<Home />} />
+            <Route path="/login" element={<Login />} />
           </>
         ) : (
-          // If someone tries to go to /admin on the main site, send them to Space
           <>
-            <Route path="/login" element={<Navigate to="/" />} />
+            {/* On the main site, the root is the Home page */}
+            <Route path="/" element={<Home />} />
+            {/* Block /admin and /login on the public site */}
             <Route path="/admin" element={<Navigate to="/" />} />
+            <Route path="/login" element={<Navigate to="/" />} />
           </>
         )}
 
-        {/* CATCH-ALL */}
+        {/* CATCH-ALL / UTILITY */}
+        <Route path="/services" element={<GenericPage title="SERVICES" color="#6222d8" />} />
+        <Route path="/community" element={<GenericPage title="COMMUNITY" color="#ff5bb8" />} />
+        <Route path="/shop" element={<GenericPage title="SHOP" color="#ff0000" />} />
+        <Route path="/blog" element={<GenericPage title="BLOG" color="#ffde00" />} />
+        <Route path="/contact" element={<GenericPage title="CONTACT" color="#90e937" />} />
+        <Route path="/prsm" element={<GenericPage title="PRSM" color="#000000" />} />
+        
         <Route path="*" element={<Space />} />
       </Routes>
     </Router>
